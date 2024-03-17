bf0a181dba78c880fdbbf14e4736149647444bae
Vinicius Kremer Santos <vinicius.kremer@gmail.com>
2024-03-17 09:22:51 -0300

    [#7] Implementando verificação de ambiente

--- functions/apis/checkouts/index.js
+++ functions/apis/checkouts/index.js
@@ -14,17 +14,27 @@ app.use(cors({ origin: true }));
 
 app.post("/", async (req, res) => {
   logger.info("Iniciando criação de checkout.");
+  let apiUrl = process.env.SANDBOX_API_PAGSEGURO;
+  let token = process.env.SANDBOX_TOKEN_PAGSEGURO;
+
+  if (req.body.isProduction) {
+    apiUrl = process.env.API_PAGSEGURO;
+    token = process.env.TOKEN_PAGSEGURO;
+  }
+
+  console.log(apiUrl);
+  console.log(token);
 
   await db
     .collection("checkouts")
     .add(req.body)
     .then(() => {
       let clientServerOptions = {
-        uri: `${process.env.SANDBOX_API_PAGSEGURO}/checkouts`,
-        body: JSON.stringify(req.body),
+        uri: `${apiUrl}/checkouts`,
+        body: JSON.stringify(req.body.checkout),
         method: "POST",
         headers: {
-          Authorization: `Bearer ${process.env.SANDBOX_TOKEN_PAGSEGURO}`,
+          Authorization: `Bearer ${token}`,
           "Content-Type": "application/json",
         },
       };
